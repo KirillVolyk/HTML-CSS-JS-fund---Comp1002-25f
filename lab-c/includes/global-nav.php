2074252501c72f1dce9ddbd683e4aefbc87f2e12
KirillVolyk <200638948@student.georgianc.on.ca>
2025-11-01 01:54:43 +0000

    added green blocks page

--- lab-c/includes/global-nav.php
+++ lab-c/includes/global-nav.php
@@ -8,10 +8,11 @@
 					<li><a href="/lab-c/yellow.php" title="Learn about yellow blocks">Yellow Blocks</a></li>
 					<li><a href="/lab-c/green.php" title="Learn about green blocks">Green Blocks</a></li>
 					<li><a href="/lab-c/about.php" title="Learn more about us">About</a></li>
+					<li><a href="/lab-c/green.php" title="Learn about green blocks">Green Blocks</a></li>
 				</ul>
 			</nav>
 <!-- STEP 4: Modify all of the above href relative paths so that they start with '/' - this means that they all point to the host name, and that the files reside in the site's public directory DONE -->
 
 <!-- STEP 5: Save this modified global-nav.php file and upload it to the server, then proceed back to index.php for STEP 6 -->
-<!-- STEP 10: Add a new link to the new green.php page inside the above list of links, save this file, upload it to the server - see how in the browser ALL site pages now feature the new link -->
+<!-- STEP 10: Add a new link to the new green.php page inside the above list of links, save this file, upload it to the server - see how in the browser ALL site pages now feature the new link DONE -->
 <!-- -->
