f99b092dc9cc6d08b26534ab5822db86a2a273ad
KirillVolyk <200638948@student.georgianc.on.ca>
2025-11-02 23:48:11 +0000

    started to work on index.php and made more files/folders

---
+++ assignment-1/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Doughjo Pizza | Master The Art Of The Slice</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="css/basics.css" />
+	</head>
+	<body>
+        <header>
+            <h1>Doughjo Pizza</h1>
+            <h2>Master The Art Of The Slice</h2>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li> <!-- were here(purpose: to introduce the website, and show some images to amke the user order) -->
+                <li><a href="menu.php">Menu</a></li> <!-- a page where you can see products(purpose: to showcase all the pizza types, sizes, shapes, etc... WIll also forward the user at the end to the order page) -->
+                <li><a href="order.php">Order</a></li> <!-- a page where you order(purpose: to order, after viewing the products you can select everything there is about a pizza + redirects to confimation page after the order) -->
+            </ul>
+        </nav>
+        <main>
+            <h2>Welcome to Doughjo Pizza!</h2>
+            <p>At Doughjo Pizza, we believe that pizza is more than just food; it's an art form. Our master chefs craft each slice with precision and passion, using only the freshest ingredients to create a symphony of flavors that will tantalize your taste buds.</p>
+	</body>
+</html>
