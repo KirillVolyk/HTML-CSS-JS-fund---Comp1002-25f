a6637683a848f709cb7d641cc827985f9e37cf15
KirillVolyk <200638948@student.georgianc.on.ca>
2025-11-03 00:43:51 +0000

    found out a way to divide index page into two: left and right, added some visuals like the border

--- assignment-1/index.php
+++ assignment-1/index.php
@@ -19,8 +19,26 @@
             <h2>Master The Art Of The Slice</h2>
         </header>
         <main>
-            <h2>Welcome to Doughjo Pizza!</h2>
-            <p>At Doughjo Pizza, we believe that pizza is more than just food; it's an art form. Our master chefs craft each slice with precision and passion, using only the freshest ingredients to create a symphony of flavors that will tantalize your taste buds.</p>
+            <div class="container">
+                <div class="left">
+                    <h2>Welcome to Doughjo Pizza!</h2>
+                    <p>At Doughjo Pizza, we believe that pizza is more than just food; it's an art form. Our master chefs craft each slice with precision and passion, using only the freshest ingredients to create a symphony of flavors that will tantalize your taste buds.</p>
+                
+                    <div class="image-block">
+                        <img src="images/doughjo-removebg-preview.png" alt="Doughjo Pizza Mascot" />
+                    </div>
+                </div>
+                <div class="right">
+                    <div class="image-block">
+                        <img src="images/pizza1.jpg" alt="Pepperoni Pizza" />
+                        <p>Pepperoni perfection with crispy crust.</p>
+                    </div>
+                    <div class="image-block">
+                        <img src="images/pizza2.jpg" alt="Veggie Pizza" />
+                        <p>Fresh veggies and gooey cheese.</p>
+                    </div>
+                </div>
+            </div>
         </main>
         <footer>
         </footer>
