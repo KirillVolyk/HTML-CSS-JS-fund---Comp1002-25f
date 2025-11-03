8cf0c80bb61bea7122edefa5272b85ebac67b745
KirillVolyk <200638948@student.georgianc.on.ca>
2025-11-03 01:34:21 +0000

    added favicon, pizza images for index, and polished navigation bar (pretty much index is done, moving onto order)

--- assignment-1/index.php
+++ assignment-1/index.php
@@ -7,10 +7,9 @@
         <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" /> <!-- adding a favicon to the page(made by copilot) -->
 	</head>
 	<body>
-        <nav>
+        <nav id="navbar">
             <ul>
                 <li><a href="index.php">Home</a></li> <!-- were here(purpose: to introduce the website, and show some images to amke the user order) -->
-                <li><a href="menu.php">Menu</a></li> <!-- a page where you can see products(purpose: to showcase all the pizza types, sizes, shapes, etc... WIll also forward the user at the end to the order page) -->
                 <li><a href="order.php">Order</a></li> <!-- a page where you order(purpose: to order, after viewing the products you can select everything there is about a pizza + redirects to confimation page after the order) -->
             </ul>
         </nav>
@@ -25,17 +24,30 @@
                     <p>At Doughjo Pizza, we believe that pizza is more than just food; it's an art form. Our master chefs craft each slice with precision and passion, using only the freshest ingredients to create a symphony of flavors that will tantalize your taste buds.</p>
                 
                     <div class="image-block">
-                        <img src="images/doughjo-removebg-preview.png" alt="Doughjo Pizza Mascot" />
+                        <img src="images/doughjo-removebg-preview.png" alt="Doughjo Pizza Mascot" width=700 height=700/>
                     </div>
                 </div>
                 <div class="right">
+                    <h2>Fan Favourites!</h2>
                     <div class="image-block">
-                        <img src="images/pizza1.jpg" alt="Pepperoni Pizza" />
+                        <img src="images/pepperoni-pizza.png" alt="Pepperoni Pizza" width=500 height=500 />
+                        <!-- https://www.generalmillsfoodservice.com/recipes/pepperoni-vodka-pizza-with-chili-crisps/1af437bc-5245-4f96-9df2-5449c0c22920 -->
                         <p>Pepperoni perfection with crispy crust.</p>
                     </div>
                     <div class="image-block">
-                        <img src="images/pizza2.jpg" alt="Veggie Pizza" />
-                        <p>Fresh veggies and gooey cheese.</p>
+                        <img src="images/four-cheese-pizza.png" alt="Four Cheeses Pizza" width=500 height=500 />
+                        <!-- https://nz.ooni.com/blogs/recipes/four-cheese-pizza -->
+                        <p>Perfected cheese combinations for you.</p>
+                    </div>
+                    <div class="image-block">
+                        <img src="images/meat-lover-pizza.png" alt="Meat Lover Pizza" width=500 height=500 />
+                        <!-- https://thatdemowebsite.com/product/meat-lovers/ -->
+                        <p>Why limit yourself to one type of meat when you can enjoy all!</p>
+                        <nav>
+                            <ul>
+                                <li><a href="order.php">Order now!</a></li>
+                            </ul>
+                        </nav>
                     </div>
                 </div>
             </div>
