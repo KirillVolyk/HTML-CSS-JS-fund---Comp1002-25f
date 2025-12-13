efb622997386d91651db66834b9a1f3cf0e51a45
Kirill.V <kirillvolik12052007@gmail.com>
2025-12-13 18:18:10 -0500

    have 6 pages set up, finished index and corporate, made some basic css, and added js for image gallery

---
+++ project-1/scripts/script.js
@@ -0,0 +1,19 @@
+// Image Gallery JavaScript
+// This code inspired by the book, DOM Scripting by Jeremy Keith (http://domscripting.com/book/)
+document.addEventListener("DOMContentLoaded", function(){
+    const gallery = document.querySelector("#imageGallery");
+    const links = gallery.querySelectorAll("#imageGallery a");
+    console.log(links);
+    for (let i = 0; i < links.length; i++) {
+        links[i].addEventListener("click", function(event){
+            let source = links[i].getAttribute("href");
+            const placeholder = document.querySelector("#placeHolder");
+            placeholder.setAttribute("src", source);
+            let text = links[i].getAttribute("title");
+            console.log(text);
+            const description = document.querySelector("#placeHolderDescription");
+            description.textContent = text;
+            event.preventDefault();
+        })
+    }
+})
