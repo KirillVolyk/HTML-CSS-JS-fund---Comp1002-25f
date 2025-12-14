ea55512030fac61118ac4fc58001d03af389e61e
Kirill.V <kirillvolik12052007@gmail.com>
2025-12-13 21:01:50 -0500

    Polished some things, added more comments, and I think I'm done

--- project-1/scripts/script.js
+++ project-1/scripts/script.js
@@ -1,11 +1,11 @@
 // Image Gallery JavaScript
 // This code inspired by the book, DOM Scripting by Jeremy Keith (http://domscripting.com/book/)
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     const gallery = document.querySelector("#imageGallery");
     const links = gallery.querySelectorAll("#imageGallery a");
     console.log(links);
     for (let i = 0; i < links.length; i++) {
-        links[i].addEventListener("click", function(event){
+        links[i].addEventListener("click", function (event) {
             let source = links[i].getAttribute("href");
             const placeholder = document.querySelector("#placeHolder");
             placeholder.setAttribute("src", source);
